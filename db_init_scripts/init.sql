ff5106f36ec6d87933507e0070eccb2a07f71d70
Sebastian <sebastian_suarez87@yahoo.com>
2025-04-22 22:18:24 -0400

    added so database get loaded everytime

--- db_init_scripts/init.sql
+++ db_init_scripts/init.sql
@@ -1,18 +1,20 @@
 CREATE DATABASE IF NOT EXISTS bank_db;
 USE bank_db;
 
+-- Create users table
 CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(50) UNIQUE NOT NULL,
+  password VARCHAR(255) NOT NULL
 );
 
+-- Create accounts table
 CREATE TABLE IF NOT EXISTS accounts (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT,
-    account_type INT,
-    balance DECIMAL(10, 2),
-    FOREIGN KEY (user_id) REFERENCES users(id)
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  user_id INT NOT NULL,
+  account_type INT DEFAULT 0,
+  balance DOUBLE DEFAULT 0.0,
+  FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 CREATE USER IF NOT EXISTS 'your_db_user' IDENTIFIED BY 'your_db_password';
